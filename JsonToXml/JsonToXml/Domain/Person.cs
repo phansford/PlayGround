b6bf1e49abe5658a9527808a183c1a7dd881e053
Paul.Hansford <Paul.Hansford@dealogic.com>
2016-11-07 17:39:16 +0000

    Added solution

---
+++ JsonToXml/JsonToXml/Domain/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonToXml.Domain
+{
+    /// <summary>
+    /// A representation of a person
+    /// </summary>
+    [DataContract]
+    internal class Person
+    {
+        /// <summary>
+        /// The Name of the person
+        /// </summary>
+        [DataMember(Name = "Name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The Date of birth for the person
+        /// </summary>
+        [DataMember(Name ="Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
